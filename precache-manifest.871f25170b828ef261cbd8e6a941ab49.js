34aa9845ce4b1287d578cced89b03cc51dc3c1ff
Vladyslav Vasylenko <vladyslav.vasylenko@dev.pro>
2020-08-12 21:32:04 +0300

    Updates

---
+++ precache-manifest.871f25170b828ef261cbd8e6a941ab49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee4d06ef2aed64012fe7842c043a4d4",
+    "url": "/Food-App/index.html"
+  },
+  {
+    "revision": "a0d24fd1f2aff3b8a11e",
+    "url": "/Food-App/static/css/main.b22e9412.chunk.css"
+  },
+  {
+    "revision": "74c686cdabe781dbf957",
+    "url": "/Food-App/static/js/2.417842bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Food-App/static/js/2.417842bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d24fd1f2aff3b8a11e",
+    "url": "/Food-App/static/js/main.8034c173.chunk.js"
+  },
+  {
+    "revision": "69be39b8d6c24da2c98e",
+    "url": "/Food-App/static/js/runtime-main.efb3cf75.js"
+  }
+]);
